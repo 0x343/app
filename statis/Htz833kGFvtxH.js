a1c3e1948fd433a8dbeb4d0ac5e40569e444c99e
0x343 <0x8567@gmail.com>
2025-07-18 12:22:35 +0700

    Delete statis/Htz833kGFvtxH.js

--- statis/Htz833kGFvtxH.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("AnonymousXXX").innerHTML = `
-<div class="video-container">
-    <center><video src="video.mp4" autoplay muted loop></video></center>
-  </div>
-<center><button class="btn-neon-electric" onclick="window.location.href='index'">
-    <span>Back to Home</span>
-  </button></center>
-`;
