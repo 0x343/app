148d6e421f227013d8982b6e1acb6d57770ef863
0x343 <0x8567@gmail.com>
2025-07-18 12:15:48 +0700

    Update html.js

--- statis/html.js
+++ statis/html.js
@@ -1,3 +1,8 @@
-document.getElementById("AXHgf753hfdxzsH").innerHTML = `
-
+document.getElementById("AnonymousXXX").innerHTML = `
+<div class="video-container">
+    <center><video src="video.mp4" autoplay muted loop></video></center>
+  </div>
+<center><button class="btn-neon-electric" onclick="window.location.href='index'">
+    <span>Back to Home</span>
+  </button></center>
 `;
